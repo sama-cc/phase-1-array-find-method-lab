8b3b30f82f7110af56137ae5a65cfa190fc0c8dc
Sam <sam.camhi@gmail.com>
2023-04-01 20:11:31 -0400

    Finally passed Array Find Method

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const superbowlWin = (array) => {
+    if (array.find(winresult => winresult.result === "W") !== undefined) {
+        return array.find(winresult => winresult.result === "W").year;
+    } else {
+        return undefined;
+    }
+}
+
+
+/*const superbowlWin = (array) => {
+    return array.find(winresult => winresult.result === "W") ? array.find(winresult => winresult.result === "W").year : undefined;
+}
+*/
